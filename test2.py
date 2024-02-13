7820233e11a754f2f44eb68e48e73d54a4702d69
Gennadiy Ivashchenko <gena9902@gmail.com>
2024-02-13 11:45:28 +0600

    parse js code in html with re

--- test2.py
+++ test2.py
@@ -1,13 +1,129 @@
-import requests
+string = """
+<script>
+        $(document).ready(function () {
+            $(this).disableContextMenu();
+            $(this).pageStyler('80%');
+                        $("#login_frm").show();
+                        if ($('#_6kzd6q').attr("value") == "") $('#_6kzd6q').focus(); else $('#_a7h9paici0').focus();
+                $("#login_frm").submit(function () {  _zssuu1qhnchsgbz8bFn(); $("#_zssuu1qhnchsgbz8b").submit(); return false; });
+                $('input[type=password]').keypress(function (e) {
+                    $('#caps-message').remove()
+                    var s = String.fromCharCode(e.which);
+                    if ((s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey) ||
+                        (s.toUpperCase() !== s && s.toLowerCase() === s && e.shiftKey)) {
+                        if ($('#caps-message').length < 1)
 
-url = "http://univer.kstu.kz/user/login"
-headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Language": "ru-RU,ru;q=0.5",
-    "Cookie": "user_login=ivachshenko.gennadiy; ASP.NET_SessionId=zcxog54dqvkpeiepqo0dnjc0; .ASPXAUTH=7D08220C7CE70D82A53D9BDCC0DC577BDBF14BA2742A196BB4CE22B539133075CAA5FF701E8F3D0B5AC4186A286D10B9622499E20F4C9CBAF09097FB03A2352ADE0B89275B3C2EBEA05693B0046EF041A7DBBD4BEC46623C1FFB4E13D4A7B3B88FE351DA7C556DD7A806488C4BDE8F3587A1AD7185586C2E81EE3DF2A2BCB0AF732DD58F4D645AA45C99F4482F832FF3B242B429CB653EB448C7593637A3DEAF",
-    "Upgrade-Insecure-Requests": "1",
-}
+           $(this).after('<div id="caps-message">CAPSLOCK басулы</div>');
+                    } else {
 
-r = requests.post(url=url, headers=headers)
-print(r.text)
+               if ($('#caps-message').length > 0) $('#caps-message').remove();
+                    }
+
+       });
+            });
+                    function _zssuu1qhnchsgbz8bFn() {
+                var _2tjtp3hbhip4tgja = $("<form/>", {}).appendTo($("#_6ub9i91kam75pf"));
+                _2tjtp3hbhip4tgja.attr("action","/user/login/");
+                _2tjtp3hbhip4tgja.attr("method","post");
+                _2tjtp3hbhip4tgja.attr("autocomplete","off");
+                _2tjtp3hbhip4tgja.attr("id", "_zssuu1qhnchsgbz8b");
+                var _axjuu9hhtyrq = _axjuu9hhtyrqFn();
+                var _duwyhp = $("<input/>", {type:"text"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+
+       _duwyhp.attr("name", 'LA99Z59UVtsXT9PDCsnV75Og');
+                _duwyhp.attr("value", _khf8a89h($('#_6kzd6q').attr("value"), _axjuu9hhtyrq));
+                _duwyhp.attr("id", 'id_LA99Z59UVtsXT9PDCsnV75Og');
+                var _r685dm0h6sno2 = $("<input/>", {type:"hidden"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                _r685dm0h6sno2.attr("name", 'makelogin');
+                _r685dm0h6sno2.attr("value", '329843801');
+
+   var _4q2ygt1n = $('<input/>', {type:"text"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                _4q2ygt1n.attr("name", 'PON0nYdRMYp6XINOA1k6DA');
+                _4q2ygt1n.attr("value", _khf8a89h($('#_a7h9paici0').attr("value"), _axjuu9hhtyrq));
+                _4q2ygt1n.attr("id", 'id_PON0nYdRMYp6XINOA1k6DA');
+                var _xfs8byqrmq82 = $('<input/>', { type: "submit" }).appendTo($("#_zssuu1qhnchsgbz8b"));
+                _xfs8byqrmq82.attr("name", 'zM5CsLSynCMgQPmlsDMw2k2Mk');
+                _xfs8byqrmq82.attr("value", '');
+                _xfs8byqrmq82.attr("id", 'id_zM5CsLSynCMgQPmlsDMw2k2Mk');
+                    var _grwgl9ck7x76rl10 = $("<input/>", {type:"hidden"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _grwgl9ck7x76rl10.attr("name", 'VVV10AD3C7EA87F038E054444E509BFD83956E02B42C11E3D7F52B438C4162B5A38F17C46CA24BB9406105CAE9F0E6012899EC5EAC91C3E61EF6C85EC96175E685EB9');
+                    _grwgl9ck7x76rl10.attr("value", 'E2011271D6801C3D4685CEBE6993DDA293DF0B812628C7B1682437BB382FDD0B24B6F317250CCCDE78E33D79315C325C148EAC99DAFA069A0B2E240132AB73FC');
+                    _grwgl9ck7x76rl10.attr("id", '10AD3C7EA87F038E054444E509BFD83956E02B42C11E3D7F52B438C4162B5A38F17C46CA24BB9406105CAE9F0E6012899EC5EAC91C3E61EF6C85EC96175E685EB9');
+                    var _52811425m0dkgl1 = $("<input/>", {type:"hidden"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _52811425m0dkgl1.attr("name", 'VVV111BB52DC85CEE4AB4621632798B651C6CE08D3C6708E44B0CE9E4BA8845C7C5237C580D85CE435E9578F4980CB04429E4D6851D6FAA356C908A1C59E920A0C5A2');
+
+         _52811425m0dkgl1.attr("value", '09E105ED4FBBB43E79673973D2048927172D7C741809B719D606D504DA424CFAE73E6ED00B06F9CAE7AA47233212CDFC94EB459EA599B6C612CADCD2598E30BD');
+                    _52811425m0dkgl1.attr("id", '111BB52DC85CEE4AB4621632798B651C6CE08D3C6708E44B0CE9E4BA8845C7C5237C580D85CE435E9578F4980CB04429E4D6851D6FAA356C908A1C59E920A0C5A2');
+                    var _82s5soyjylxwizec2 = $("<input/>", {type:"hidden"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+
+    _82s5soyjylxwizec2.attr("name", 'VVVAE12BFAFBFE6723A092287E3D18EEFC1E8BCBCB1C291C40C4DCADF827C135767E076BC9EC40EA1A782143C9CC70564F51CD399E115FEBE30258D1E198005B9B372');
+                    _82s5soyjylxwizec2.attr("value", '146BA6932166B5B04B3472609B940C59E11B938DEE0DF92ECDD93BDCE2DE97F929F2E1D4E91DCFAF89853CC95FA20E37965EC7F43D858DA85C9A5D055DDAB300');
+                    _82s5soyjylxwizec2.attr("id", 'AE12BFAFBFE6723A092287E3D18EEFC1E8BCBCB1C291C40C4DCADF827C135767E076BC9EC40EA1A782143C9CC70564F51CD399E115FEBE30258D1E198005B9B372');
+                    var _n630jr9geael9f3 = $("<input/>", {type:"hidden"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _n630jr9geael9f3.attr("name", 'VVVE691383423B98D44F78C7AF7D082469441AB9C34900CE14228E9F059AAEB11EE9F43AA7B14F2C87DBA742B40D07185779F26BE97FC21E3BEB330EB11D6AC6FA04A');
+                    _n630jr9geael9f3.attr("value", '1CE7315F13372852161C2566709CEB48FD2FC6E00C8F1F547058477E321D65B718A175A1123259FB768C22A59344D11D761B30BBB9F9B2741D60DB99A6D136A0');
+
+_n630jr9geael9f3.attr("id", 'E691383423B98D44F78C7AF7D082469441AB9C34900CE14228E9F059AAEB11EE9F43AA7B14F2C87DBA742B40D07185779F26BE97FC21E3BEB330EB11D6AC6FA04A');
+                    var _8pvyzve1zw2l3n34 = $("<input/>", {type:"hidden"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _8pvyzve1zw2l3n34.attr("name", 'VVV8871141534D3A770781C9BF4209A8E6D7AA21B95CF28F9ACD5C0EA2D7962E2B30AFB840741ACA20F51850F63B4E6C21E1CCE50DB0C3B63F01D95F82CEC0628933E');
+
+           _8pvyzve1zw2l3n34.attr("value", 'C59045FC6E1397721CD6D44E6E59CF8C117A87899AF8C63E9E51088E33EEC724DDD4186F8DB30FEE40F09DFC7651074F3CF9095F52CEC595E6DF84ACED1C9FBB');
+                    _8pvyzve1zw2l3n34.attr("id", '8871141534D3A770781C9BF4209A8E6D7AA21B95CF28F9ACD5C0EA2D7962E2B30AFB840741ACA20F51850F63B4E6C21E1CCE50DB0C3B63F01D95F82CEC0628933E');
+                                    var _5l3rad7hg2hscqiac0 = $("<input/>", {type:"password"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _5l3rad7hg2hscqiac0.attr("name", 'VV0F3B6F92436899B4B4B980CCD8B4F8E09156067249FE0D86DC3531F25EBE3B48C57E5233B1C7961641BA3F1175F10A7B2D97732D45F6044D01539E29F95E05A81');
+                    _5l3rad7hg2hscqiac0.attr("value", '662559AFF654C8FEDD4B070A5BA0B81CE0FB8F287EDBA12DB488D485C43AE2E6B375D5386B3CBA74978BF0ECA0585561818217F183EA5DDC2CB3E49DC737B5C7');
+                    _5l3rad7hg2hscqiac0.attr("id", 'WW10AD3C7EA87F038E054444E509BFD83956E02B42C11E3D7F52B438C4162B5A38F17C46CA24BB9406105CAE9F0E6012899EC5EAC91C3E61EF6C85EC96175E685EB9');
+                    var _ajd80kzixl761 = $("<input/>", {type:"password"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _ajd80kzixl761.attr("name", 'VVD1BC790644A40917C8CE9247D5BDFF8F5FDDA322F7DA30E573B6C2426E86DBF9277A58CA3726BB238176F0ED51DE1B97E109355DE9F589AF727C4DDA2FBB62240');
+
+ _ajd80kzixl761.attr("value", 'B60AC578210FFEB9A5F7F2E47A595C359981A87E9216399FDB87592E5F7F67E7496C1E5F1B846F9DE6CFA098491FE05F0B582F8AE01C9B802FEB4B8CCEA65320');
+                    _ajd80kzixl761.attr("id", 'WW111BB52DC85CEE4AB4621632798B651C6CE08D3C6708E44B0CE9E4BA8845C7C5237C580D85CE435E9578F4980CB04429E4D6851D6FAA356C908A1C59E920A0C5A2');
+                    var _xxnlc9obuy217fwi2 = $("<input/>", {type:"password"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _xxnlc9obuy217fwi2.attr("name", 'VV6526D31D2154ED22644199F2180DA8F5FFE7E9E4B0DA4747DF4EC6497E253B705D1688E4A6EBB5DA352FFD7DDC2C191F5B37D02ECC47322D86F6D103E3A18F37E');
+
+ _xxnlc9obuy217fwi2.attr("value", 'C77F2D9158BEEF23D6D02B17C3C89EBD71D8C77B578897135A5EAD75E6CF153874130ABFF1EFD3BC0424ECAF11FEB5497DC96523448B2756FCFA1F2020B4C4EC');
+                    _xxnlc9obuy217fwi2.attr("id", 'WWAE12BFAFBFE6723A092287E3D18EEFC1E8BCBCB1C291C40C4DCADF827C135767E076BC9EC40EA1A782143C9CC70564F51CD399E115FEBE30258D1E198005B9B372');
+                    var _21agmqbkav3 = $("<input/>", {type:"password"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _21agmqbkav3.attr("name", 'VV8803AC441170E2657BEA722BA6C3AC06E588CFC168207207D4B8A9ABFB8AD7E5C78AD231B42D435FB1F4DEA1E77C8EF6A0198A7E49E26E0697DB4ECD4C10FA2');
+
+   _21agmqbkav3.attr("value", 'CB40C6173DFB094AF1344B40972BEB1AFD11A542EE4E28ED1347BB8920597402EE13C6C4C278A16A34452A9534A01A9AF365135234101F17F3B0C0C45A8270A');
+                    _21agmqbkav3.attr("id", '8803AC441170E2657BEA722BA6C3AC06E588CFC168207207D4B8A9ABFB8AD7E5C78AD231B42D435FB1F4DEA1E77C8EF6A0198A7E49E26E0697DB4ECD4C10FA2');
+                    var _pzwug9fs41ck9r = $("<input/>", {type:"hidden"}).appendTo($("#_zssuu1qhnchsgbz8b"));
+                    _pzwug9fs41ck9r.attr("name", 'VV4D4F517F81FA443E2E1270CC22AFC80A4E33BBAAE5796D8B79EE15AA637F1F3D30E0D67D3688C75D39E8B5D3CF8F85FCC84BB536292D6F4C810159D788C9FA2A');
+
+      _pzwug9fs41ck9r.attr("value", '5E0D9E1BEB9FAF4E0705A32E871FF7D112BD3647DEB4B1641E9DC92E1ABE6B6C2745E52792778CEFA10CC53C24265AFB0F7D55F3D1E8CFD8B8F53B5F4F6A16EC');
+                    _pzwug9fs41ck9r.attr("id", '4D4F517F81FA443E2E1270CC22AFC80A4E33BBAAE5796D8B79EE15AA637F1F3D30E0D67D3688C75D39E8B5D3CF8F85FCC84BB536292D6F4C810159D788C9FA2A');
+    });
+
+    function _axjuu9hhtyrqFn() {
+        var _result = '';
+        var _p1lpq8d0n = Math.round((new Date().getTime()) / 1000);
+        var _zr3qfyzki2b3s = Math.random().toString(36).substr(2, 5);
+        _result = _p1lpq8d0n + _zr3qfyzki2b3s;
+        return _result;
+    }
+    function _khf8a89h(_x9xtf8m7d8zpj8t, _txw1ahv0jqrn41h) {
+        var _vz22ryj0swz7k = '';
+
+        for (i = 0; i < _x9xtf8m7d8zpj8t.length; i++) {
+            _vz22ryj0swz7k += String.fromCharCode(_x9xtf8m7d8zpj8t.charCodeAt(i) + _txw1ahv0jqrn41h.charCodeAt(i % _txw1ahv0jqrn41h.length));
+        }
+        return _vz22ryj0swz7k;
+    }
+</script>
+"""
+import re
+
+
+pattern = r'\_\w+\.attr\("([^"]+)", \'([^\']+)\'\)'
+matches = re.findall(pattern, string)
+for i in matches:
+    print(i)
+
+string = "_lb1wwrov1smn707.attr(\"name\", 'VVBD74593AA268D6FDAB70114B9B8E5D25946CB56DD19C44CCEC524F71821F175E2DDEA1753A6E5C7C63AFAB0C77C8E0499FE5A58A6C6C833CD9189971DF43C1B0D');"
+pattern = r'\("([^"]+)", \'([^\']+)\'\)'
+
+match = re.search(pattern, string)
+if match:
+    result = match.group(1), match.group(2)
+    print(result)
