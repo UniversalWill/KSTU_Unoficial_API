1a16847405b5464073e6f5bfb01134e9e3c841b3
Gennadiy Ivashchenko <gena9902@gmail.com>
2024-02-17 17:01:34 +0600

    add typing and several exception

--- Services/SheduleService.py
+++ Services/SheduleService.py
@@ -1,58 +1,92 @@
+import json
+from typing import Dict, Optional
 from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientError
 from bs4 import BeautifulSoup
-
 from Services.UserService import User
 
+ScheduleData = Dict[str, Dict[str, Optional[Dict[str, str]]]]
 
-async def get_shedule(login, password):
-    user = User(login, password)
-    url = "http://univer.kstu.kz/student/myschedule/"
-    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "ru-RU,ru;q=0.5",
-        "Referer": "http://univer.kstu.kz/student/bachelor/",
-        "Upgrade-Insecure-Requests": "1",
-    }
 
-    async with ClientSession() as session:
-        dict_cookie = await user.fetch_cookies(session)
-        if dict_cookie:
-            async with session.get(
-                url, headers=headers, cookies=dict_cookie
-            ) as response:
-                schedule_data = {}
-                soup = BeautifulSoup(await response.text(), "html.parser")
-                table_element = soup.find("table", class_="schedule")
+async def get_shedule(login: str, password: str) -> str | None:
+    try:
+        user: User = User(login, password)
+        url: str = "http://univer.kstu.kz/student/myschedule/"
+        headers: dict[str, str] = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "ru-RU,ru;q=0.5",
+            "Referer": "http://univer.kstu.kz/student/bachelor/",
+            "Upgrade-Insecure-Requests": "1",
+        }
 
-                for day in days:
-                    schedule_data[day] = {}
-                    day_column_index = days.index(day) + 1
-                    rows = table_element.select(
-                        f"tr td:nth-child({day_column_index}) div.groups"
+        async with ClientSession() as session:
+            dict_cookie: Dict[str, str] | None = await user.fetch_cookies(session)
+            if dict_cookie:
+                async with session.get(
+                    url, headers=headers, cookies=dict_cookie
+                ) as response:
+                    soup = BeautifulSoup(await response.text(), "html.parser")
+                    schedule_data: ScheduleData = {}
+
+                    # Получаем дни недели
+                    days_of_week = soup.find(
+                        "tr", {"style": "height: 20px; border:1px solid Green"}
+                    ).find_all("th")
+                    days = [
+                        day.text.strip() for day in days_of_week if day.text.strip()
+                    ]
+
+                    for day in days:
+                        schedule_data[day] = {}
+
+                        rows = soup.select(
+                            f"tr td.field:nth-child({days.index(day) + 2}) div.groups"
+                        )
+
+                        for row in rows:
+                            time = row.find_previous("td", class_="time").text.strip()
+                            course = row.find("p", class_="teacher").text.strip()
+                            instructor = (
+                                row.find("p", class_="teacher")
+                                .find_next("p", class_="teacher")
+                                .text.strip()
+                            )
+                            location = (
+                                row.find("span", class_="aud_faculty")
+                                .find_next("span")
+                                .text.strip()
+                            )
+
+                            # Определение числитель/знаменатель
+                            denominator_tag = row.find("p", class_="denominator")
+                            denominator = (
+                                "Числитель" if denominator_tag else "Знаменатель"
+                            )
+
+                            schedule_data[day][time] = {
+                                "subject": course,
+                                "teacher": instructor,
+                                "classroom": location,
+                                "period": "Период с 15.01 по 20.03",
+                                "denominator": denominator
+                                if denominator_tag
+                                else "Каждая неделя",
+                            }
+
+                    # Сериализация в JSON и вывод
+                    serialized_schedule = json.dumps(
+                        schedule_data, ensure_ascii=False, indent=2
                     )
 
-                    for row in rows:
-                        time = row.find_previous("td", class_="time").text.strip()
-                        course = row.find("p", class_="teacher").text.strip()
-                        instructor = row.select_one(
-                            "p.teacher ~ p.teacher"
-                        ).text.strip()
-                        location = row.select_one(
-                            "span.aud_faculty + span"
-                        ).text.strip()
-                        denominator_tag = row.find("span", class_="denominator")
-                        denominator = (
-                            denominator_tag.text.strip()
-                            if denominator_tag
-                            else "Каждая неделя"
-                        )
-                        schedule_data[day][course] = {
-                            "time": time,
-                            "type": course.split("(")[1][:-1],
-                            "instructor": instructor,
-                            "location": location,
-                            "denominator": denominator,
-                        }
-                return schedule_data
+                    return serialized_schedule
+
+    except ClientError as e:
+        # Handle ClientError (e.g., network issues)
+        print(f"Client error: {e}")
+        return None
+
+    except Exception as e:
+        # Handle other exceptions
+        print(f"Error: {e}")
+        return None
--- Services/UserService.py
+++ Services/UserService.py
@@ -1,28 +1,41 @@
-from aiohttp.client import ClientSession
-from requests.sessions import Session
+from typing import Dict
+from aiohttp.client import ClientSession, ClientResponseError
 
 
 class User:
-    def __init__(self, login: str, password: str):
+    def __init__(self, login: str, password: str) -> None:
         self.login = login
         self.password = password
-        self.session = Session()
+        self.session = ClientSession()
 
-    async def fetch_cookies(self, session: ClientSession):
-        async with ClientSession() as session:
-            async with session.get(
-                f"https://univerapi.kstu.kz/?login={self.login}&password={self.password}"
-            ) as response:
-                cookies = response.cookies
-                aspxauth_cookie = cookies.get(".ASPXAUTH")
-                sessionid_cookie = cookies.get("ASP.NET_SessionId")
-                if aspxauth_cookie and sessionid_cookie:
-                    return {
-                        ".ASPXAUTH": aspxauth_cookie.value,
-                        "ASP.NET_SessionId": sessionid_cookie.value,
-                    }
-                else:
-                    return None
+    CookieData = Dict[str, str]
+
+    async def fetch_cookies(self, session: ClientSession) -> None | Dict[str, str]:
+        try:
+            async with self.session as session:
+                async with session.get(
+                    f"https://univerapi.kstu.kz/?login={self.login}&password={self.password}"
+                ) as response:
+                    cookies = response.cookies
+                    aspxauth_cookie = cookies.get(".ASPXAUTH")
+                    sessionid_cookie = cookies.get("ASP.NET_SessionId")
+                    if aspxauth_cookie and sessionid_cookie:
+                        return {
+                            ".ASPXAUTH": aspxauth_cookie.value,
+                            "ASP.NET_SessionId": sessionid_cookie.value,
+                        }
+                    else:
+                        return None
+
+        except ClientResponseError as e:
+            # Handle ClientResponseError (e.g., HTTP errors)
+            print(f"HTTP error: {e}")
+            return None
+
+        except Exception as e:
+            # Handle other exceptions
+            print(f"Error: {e}")
+            return None
 
     # def get_payload(self):
     #     request = self.session.get("http://univer.kstu.kz/user/login")
